0a64d90ddf69047f4e0755635cb46563d29a099c
harsh <halashi20comp@student.mes.ac.in>
2024-05-01 01:19:29 +0530

    minor fixes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-noraml text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg text-slate-gray"
               >
                 {item.label}
               </a>
